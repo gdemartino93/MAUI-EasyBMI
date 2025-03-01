f1cbbacef6c58b6e2e560f6b9ea4f1c64028c9d0
gdemartino93 <g.demartino9393@gmail.com>
2025-03-01 16:47:44 +0100

    added appsettings and loaded syncFusion license key from it

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Licensing;
 using Syncfusion.Maui.Core.Hosting;
+using System.Reflection;
 
 namespace MAUI_EasyBMI
 {
@@ -16,12 +19,31 @@ namespace MAUI_EasyBMI
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
+            builder.AddAppSettings();
+            string? syncKey = builder.Configuration.GetValue<string>("SyncfusionLicenseKey");
+            if (!string.IsNullOrEmpty(syncKey))
+            {
+                SyncfusionLicenseProvider.RegisterLicense(syncKey);
+            }
 #if DEBUG
-    		builder.Logging.AddDebug();
+                builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
+        }
+        private static void AddAppSettings(this MauiAppBuilder builder)
+        {
+            using Stream? stream = Assembly.
+                GetExecutingAssembly()
+                .GetManifestResourceStream("MAUI_EasyBMI.appsettings.json");
+
+            if (stream != null)
+            {
+                IConfigurationRoot config = new ConfigurationBuilder()
+                    .AddJsonStream(stream)
+                    .Build();
+                builder.Configuration.AddConfiguration(config);
+            }
         }
     }
 }
